b7bfe6fac311357385d3e0d9b48942b8d2b7eaf4
Dominik Inführ <dinfuehr@chromium.org>
2022-01-25 18:29:08 +0100

    libjvm.so loads successfully

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,22 @@
 type jint = i32;
 
 #[warn(non_snake_case)]
+#[no_mangle]
 pub extern "C" fn JNI_CreateJavaVM() -> jint {
-    println!("reached my library!");
+    println!("in JNI_CreateJavaVM");
+    panic!("YES!");
+}
+
+#[warn(non_snake_case)]
+#[no_mangle]
+pub extern "C" fn JNI_GetDefaultJavaVMInitArgs() -> jint {
+    println!("in JNI_GetDefaultJavaVMInitArgs");
+    panic!("YES!");
+}
+
+#[warn(non_snake_case)]
+#[no_mangle]
+pub extern "C" fn JNI_GetCreatedJavaVMs() -> jint {
+    println!("in JNI_GetCreatedJavaVMs");
     panic!("YES!");
 }
